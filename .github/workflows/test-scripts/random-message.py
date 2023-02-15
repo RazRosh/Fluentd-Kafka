44d8a263a9878eb6bc28a32a17d270aa96c70894
RazRosh <47662109+RazRosh@users.noreply.github.com>
2023-02-15 22:36:15 +0200

    commit message

---
+++ .github/workflows/test-scripts/random-message.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+print(get_random_string(10))
